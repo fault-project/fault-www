11ca4346ede2d8948b9685eb71f4c4631db564ab
Sylvain Hellegouarch <sh@defuze.org>
2025-08-08 11:46:46 +0200

    add llms.txt
    Signed-off-by: Sylvain Hellegouarch <sh@defuze.org>

--- hooks.py
+++ hooks.py
@@ -5,3 +5,4 @@ import shutil
 def copy_install(config, **kwargs) -> None:
     site_dir = config['site_dir']
     shutil.copy('docs/assets/install.sh', os.path.join(site_dir, 'get'))
+    shutil.copy('docs/assets/llms.txt', os.path.join(site_dir, 'llms.txt'))
